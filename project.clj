2f02cea0e90995714291468a57e15e9ce6f7a999
Lee Hinman <lee@writequit.org>
2011-03-28 11:54:11 -0600

    remove lein-javac as a dev dependency, as it's part of leiningen now

--- project.clj
+++ project.clj
@@ -5,9 +5,8 @@
   :java-source-path "src/jvm"
   :javac-fork "true"
   :dependencies
-    [[org.clojure/clojure "1.2.1"]
-     [org.clojure/clojure-contrib "1.2.0"]
-     [org.codehaus.jackson/jackson-core-asl "1.7.4"]
-     [org.codehaus.jackson/jackson-smile "1.7.4"]]
-  :dev-dependencies
-    [[org.clojars.mmcgrana/lein-javac "1.2.1"]])
+  [[org.clojure/clojure "1.2.1"]
+   [org.clojure/clojure-contrib "1.2.0"]
+   [org.codehaus.jackson/jackson-core-asl "1.7.4"]
+   [org.codehaus.jackson/jackson-smile "1.7.4"]]
+  :dev-dependencies [])
