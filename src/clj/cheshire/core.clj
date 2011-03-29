e3334f6cf8d974dd43d1849f57cd5d2e162b8af6
Lee Hinman <lee@writequit.org>
2011-03-29 11:57:10 -0600

    clean up docstrings in the clojure code

--- src/clj/cheshire/core.clj
+++ src/clj/cheshire/core.clj
@@ -3,91 +3,115 @@
            (org.codehaus.jackson.smile SmileFactory)
            (org.codehaus.jackson JsonFactory JsonParser JsonParser$Feature)
            (java.io StringWriter StringReader BufferedReader
-                    ByteArrayOutputStream))
-  (:use (clojure.contrib [def :only (defvar-)])))
+                    ByteArrayOutputStream)))
 
+;; default date format used to JSON-encode Date objects
 (def default-date-format "yyyy-MM-dd'T'HH:mm:ss'Z'")
 
-(defvar- #^JsonFactory factory
+;; Factory objects that are needed to do the encoding and decoding
+(def ^{:private true :type JsonFactory} factory
   (doto (JsonFactory.)
     (.configure JsonParser$Feature/ALLOW_UNQUOTED_CONTROL_CHARS true)))
 
-;; Left blank here in case future options are needed
-(defvar- #^SmileFactory smile-factory
+(def ^{:private true :type SmileFactory} smile-factory
   (SmileFactory.))
 
-(defn generate-string
-  {:tag String
-   :doc "Returns a JSON-encoding String for the given Clojure object."}
+;; Generators
+(defn ^String generate-string
+  "Returns a JSON-encoding String for the given Clojure object. Takes an
+  optional date format string that Date objects will be encoded with.
+
+  The default date format (in UTC) is: yyyy-MM-dd'T'HH:mm:ss'Z'"
   [obj & [date-format]]
-  (let [sw        (StringWriter.)
+  (let [sw (StringWriter.)
         generator (.createJsonGenerator factory sw)]
     (JsonExt/generate generator obj (or date-format default-date-format))
     (.flush generator)
     (.toString sw)))
 
-(defn generate-stream
-  {:tag String
-   :doc "Returns a BufferedWriter for the given Clojure object with the.
-  JSON-encoded data written to the writer"}
-  [obj #^BufferedWriter writer & [date-format]]
+(defn ^String generate-stream
+  "Returns a BufferedWriter for the given Clojure object with the.
+  JSON-encoded data written to the writer. Takes an optional date
+  format string that Date objects will be encoded with.
+
+  The default date format (in UTC) is: yyyy-MM-dd'T'HH:mm:ss'Z'"
+  [obj ^BufferedWriter writer & [^String date-format]]
   (let [generator (.createJsonGenerator factory writer)]
     (JsonExt/generate generator obj (or date-format default-date-format))
     (.flush generator)
     writer))
 
-;; alias for clojure-json users
-(def encode generate-string)
-(def encode-stream generate-stream)
-
 (defn generate-smile
-  "Returns a SMILE-encoded byte-array for the given Clojure object."
-  [obj & [date-format]]
+  "Returns a SMILE-encoded byte-array for the given Clojure object.
+  Takes an optional date format string that Date objects will be encoded with.
+
+  The default date format (in UTC) is: yyyy-MM-dd'T'HH:mm:ss'Z'"
+  [obj & [^String date-format]]
   (let [baos (ByteArrayOutputStream.)
         generator (.createJsonGenerator smile-factory baos)]
     (JsonExt/generate generator obj (or date-format default-date-format))
     (.flush generator)
     (.toByteArray baos)))
 
+;; Parsers
 (defn parse-string
-  "Returns the Clojure object corresponding to the given JSON-encoded string."
-  [string & [keywords]]
+  "Returns the Clojure object corresponding to the given JSON-encoded string.
+  keywords? should be true if keyword keys are needed, the default is false,
+  maps will use strings as keywords."
+  [^String string & [keywords?]]
   (JsonExt/parse
    (.createJsonParser factory (StringReader. string))
-   true (or keywords false) nil))
+   true (or keywords? false) nil))
 
 (defn parse-stream
   "Returns the Clojure object corresponding to the given reader, reader must
-  implement BufferedReader."
-  [#^BufferedReader rdr & [keywords]]
+  implement BufferedReader. keywords? should be true if keyword keys are needed,
+  the default is false, maps will use strings as keywords.
+
+  If laziness is needed, see parsed-seq."
+  [^BufferedReader rdr & [keywords?]]
   (JsonExt/parse
    (.createJsonParser factory rdr)
-   true (or keywords false) nil))
-
-;; alias for clojure-json users
-(def decode parse-string)
-(def decode-stream parse-stream)
+   true (or keywords? false) nil))
 
 (defn parse-smile
-  "Returns the Clojure object corresponding to the given SMILE-encoded bytes."
-  [bytes & [keywords]]
+  "Returns the Clojure object corresponding to the given SMILE-encoded bytes.
+  keywords? should be true if keyword keys are needed, the default is false,
+  maps will use strings as keywords."
+  [bytes & [keywords?]]
   (JsonExt/parse
    (.createJsonParser smile-factory bytes)
-   true (or keywords false) nil))
+   true (or keywords? false) nil))
 
-(defn- parsed-seq* [#^JsonParser parser keywords]
+;; Lazy parsers
+(defn- parsed-seq*
+  "Internal lazy-seq parser"
+  [^JsonParser parser keywords?]
   (let [eof (Object.)]
     (lazy-seq
-     (let [elem (JsonExt/parse parser true keywords eof)]
+     (let [elem (JsonExt/parse parser true keywords? eof)]
        (if-not (identical? elem eof)
-         (cons elem (parsed-seq* parser keywords)))))))
+         (cons elem (parsed-seq* parser keywords?)))))))
 
-(defn parsed-seq [#^BufferedReader reader & [keywords]]
+(defn parsed-seq
   "Returns a lazy seq of Clojure objects corresponding to the JSON read from
-  the given reader. The seq continues until the end of the reader is reached."
-  (parsed-seq* (.createJsonParser factory reader) (or keywords false)))
+  the given reader. The seq continues until the end of the reader is reached.
 
-(defn parsed-smile-seq [#^BufferedReader reader & [keywords]]
+  If non-laziness is needed, see parse-stream."
+  [^BufferedReader reader & [keywords?]]
+  (parsed-seq* (.createJsonParser factory reader) (or keywords? false)))
+
+(defn parsed-smile-seq
   "Returns a lazy seq of Clojure objects corresponding to the SMILE read from
   the given reader. The seq continues until the end of the reader is reached."
-  (parsed-seq* (.createJsonParser smile-factory reader) (or keywords false)))
+  [^BufferedReader reader & [keywords?]]
+  (parsed-seq* (.createJsonParser smile-factory reader) (or keywords? false)))
+
+;; aliases for clojure-json users
+(def encode generate-string)
+(def encode-stream generate-stream)
+(def encode-smile generate-smile)
+(def decode parse-string)
+(def decode-stream parse-stream)
+(def decode-smile parse-smile)
+
