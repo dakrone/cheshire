6b59d8716535aa7179e3d3ab81169fedc937d002
Lee Hinman <lee@writequit.org>
2011-03-29 14:19:32 -0600

    bump to 1.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cheshire "1.1.0"
+(defproject cheshire "1.1.1-SNAPSHOT"
   :description "JSON and JSON SMILE encoding, fast."
   :url "https://github.com/dakrone/cheshire"
   :source-path "src/clj"
