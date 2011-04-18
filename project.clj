d1bb305a2d707d25c2330180a75b94fa69095548
Lee Hinman <lee@writequit.org>
2011-04-18 08:59:37 -0600

    bump jackson to version 1.7.6

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/dakrone/cheshire"
   :dependencies
   [[org.clojure/clojure "1.2.1"]
-   [org.codehaus.jackson/jackson-core-asl "1.7.4"]
-   [org.codehaus.jackson/jackson-smile "1.7.4"]]
+   [org.codehaus.jackson/jackson-core-asl "1.7.6"]
+   [org.codehaus.jackson/jackson-smile "1.7.6"]]
   :dev-dependencies [[marginalia "0.5.0"]])
