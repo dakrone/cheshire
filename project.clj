061391ed81dd52ea163a6620664a100a66d97334
Lee Hinman <lee@writequit.org>
2011-04-18 09:04:28 -0600

    and now back to 1.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cheshire "1.1.1"
+(defproject cheshire "1.1.2-SNAPSHOT"
   :description "JSON and JSON SMILE encoding, fast."
   :url "https://github.com/dakrone/cheshire"
   :dependencies
