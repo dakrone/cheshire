85f195b19b3ae207d6d90ae339f90481e295b016
Lee Hinman <lee@writequit.org>
2011-06-28 10:19:05 -0600

    add custom encoding (still a wip)

---
+++ src/cheshire/custom.clj
@@ -0,0 +1,152 @@
+(ns cheshire.custom
+  (:require [cheshire.core :as core])
+  (:import (java.io BufferedWriter ByteArrayOutputStream StringWriter)
+           (java.util Date SimpleTimeZone)
+           (java.text SimpleDateFormat)
+           (org.codehaus.jackson.smile SmileFactory)
+           (org.codehaus.jackson JsonFactory JsonGenerator JsonParser
+                                 JsonParser$Feature)))
+
+(set! *warn-on-reflection* true)
+
+(def ^{:private true :tag JsonFactory} factory
+  (doto (JsonFactory.)
+    (.configure JsonParser$Feature/ALLOW_UNQUOTED_CONTROL_CHARS true)))
+
+(def ^{:private true :tag SmileFactory} smile-factory
+  (SmileFactory.))
+
+(def ^{:dynamic true} *date-format* "yyyy-MM-dd'T'HH:mm:ss'Z'")
+
+(defprotocol Jable
+  (to-json [t jg]))
+
+(defn ^String encode [obj & [^String date-format]]
+  (binding [*date-format* (or date-format *date-format*)]
+    (let [sw (StringWriter.)
+          generator (.createJsonGenerator ^JsonFactory factory sw)]
+      (if obj
+        (to-json obj generator)
+        (.writeNull generator))
+      (.flush generator)
+      (.toString sw))))
+
+(defn ^String encode-stream [obj ^BufferedWriter w & [^String date-format]]
+  (binding [*date-format* (or date-format *date-format*)]
+    (let [generator (.createJsonGenerator factory w)]
+      (to-json obj generator)
+      (.flush generator)
+      w)))
+
+(defn encode-smile
+  [obj & [^String date-format]]
+  (binding [*date-format* (or date-format *date-format*)]
+    (let [baos (ByteArrayOutputStream.)
+          generator (.createJsonGenerator smile-factory baos)]
+      (to-json obj generator)
+      (.flush generator)
+      (.toByteArray baos))))
+
+;; there are no differences in parsing, but these are here to make
+;; this a self-contained namespace if desired
+(def parse core/decode)
+(def parse-string core/decode)
+(def parse-stream core/decode-stream)
+(def parse-smile core/decode-smile)
+(def parsed-seq core/parsed-seq)
+(def decode core/parse-string)
+(def decode-stream parse-stream)
+(def decode-smile parse-smile)
+
+;; aliases
+(def generate-string encode)
+(def generate-stream encode-stream)
+(def generate-smile encode-smile)
+
+(defn- encode-nil [_ ^JsonGenerator jg]
+  (.writeNull jg))
+
+(extend nil
+  Jable
+  {:to-json encode-nil})
+
+(defn- encode-str [^String s ^JsonGenerator jg]
+  (.writeString jg (str s)))
+
+(extend java.lang.String
+  Jable
+  {:to-json encode-str})
+
+(defn- encode-number [^java.lang.Number n ^JsonGenerator jg]
+  (.writeNumber jg n))
+
+(extend java.lang.Number
+  Jable
+  {:to-json encode-number})
+
+(defn- encode-seq [s ^JsonGenerator jg]
+  (.writeStartArray jg)
+  (doseq [i s]
+    (to-json i jg))
+  (.writeEndArray jg))
+
+(extend clojure.lang.ISeq
+  Jable
+  {:to-json encode-seq})
+
+(extend clojure.lang.IPersistentVector
+  Jable
+  {:to-json encode-seq})
+
+(extend clojure.lang.IPersistentSet
+  Jable
+  {:to-json encode-seq})
+
+(defn- encode-date [^Date d ^JsonGenerator jg]
+  (let [sdf (SimpleDateFormat. *date-format*)]
+    (.setTimeZone sdf (SimpleTimeZone. 0 "UTC"))
+    (.writeString jg (.format sdf d))))
+
+(extend java.util.Date
+  Jable
+  {:to-json encode-date})
+
+(extend java.util.UUID
+  Jable
+  {:to-json encode-str})
+
+(defn- encode-bool [^Boolean b ^JsonGenerator jg]
+  (.writeBoolean jg b))
+
+(extend java.lang.Boolean
+  Jable
+  {:to-json encode-bool})
+
+(defn- encode-named [^clojure.lang.Keyword k ^JsonGenerator jg]
+  (.writeString jg (name k)))
+
+(extend clojure.lang.Keyword
+  Jable
+  {:to-json encode-named})
+
+(defn- encode-map [^clojure.lang.IPersistentMap m ^JsonGenerator jg]
+  (.writeStartObject jg)
+  (doseq [[k v] m]
+    (.writeFieldName jg (if (instance? clojure.lang.Keyword k)
+                          (name k)
+                          (str k)))
+    (to-json v jg))
+  (.writeEndObject jg))
+
+(extend clojure.lang.IPersistentMap
+  Jable
+  {:to-json encode-map})
+
+(defn- encode-symbol [^clojure.lang.Symbol s ^JsonGenerator jg]
+  (.writeString jg (str (:ns (meta (resolve s)))
+                        "/"
+                        (:name (meta (resolve s))))))
+
+(extend clojure.lang.Symbol
+  Jable
+  {:to-json encode-symbol})
---
+++ test/cheshire/test/custom.clj
@@ -0,0 +1,113 @@
+(ns cheshire.test.custom
+  (:use [clojure.test]
+        [clojure.java.io :only [reader]])
+  (:require [cheshire.custom :as json])
+  (:import (java.io StringReader StringWriter
+                    BufferedReader BufferedWriter)
+           (java.util Date UUID)))
+
+(def test-obj {"int" 3 "long" 52001110638799097 "bigint" 9223372036854775808
+               "double" 1.23 "boolean" true "nil" nil "string" "string"
+               "vec" [1 2 3] "map" {"a" "b"} "list" (list "a" "b")})
+
+(deftest test-string-round-trip
+  (is (= test-obj (json/parse-string (json/generate-string test-obj)))))
+
+(deftest test-generate-accepts-float
+  (is (= "3.14" (json/generate-string 3.14))))
+
+(deftest test-keyword-encode
+  (is (= {"key" "val"}
+         (json/parse-string (json/generate-string {:key "val"})))))
+
+(deftest test-generate-set
+  (is (= {"set" ["a" "b"]}
+         (json/parse-string (json/generate-string {"set" #{"a" "b"}})))))
+
+(deftest test-key-coercion
+  (is (= {"foo" "bar" "1" "bat" "2" "bang" "3" "biz"}
+         (json/parse-string
+          (json/generate-string
+           {:foo "bar" 1 "bat" (long 2) "bang" (bigint 3) "biz"})))))
+
+(deftest test-keywords
+  (is (= {:foo "bar" :bat 1}
+         (json/parse-string
+          (json/generate-string {:foo "bar" :bat 1})
+          true))))
+
+(deftest test-symbols
+  (is (= {"foo" "clojure.core/map"}
+         (json/parse-string
+          (json/generate-string {"foo" 'clojure.core/map})))))
+
+(deftest test-parsed-seq
+  (let [br (BufferedReader. (StringReader. "1\n2\n3\n"))]
+    (is (= (list 1 2 3) (json/parsed-seq br)))))
+
+(deftest test-smile-round-trip
+  (is (= test-obj (json/parse-smile (json/generate-smile test-obj)))))
+
+(deftest test-aliases
+  (is (= {"foo" "bar" "1" "bat" "2" "bang" "3" "biz"}
+         (json/decode
+          (json/encode
+           {:foo "bar" 1 "bat" (long 2) "bang" (bigint 3) "biz"})))))
+
+(deftest test-date
+  (is (= {"foo" "1970-01-01T00:00:00Z"}
+         (json/decode
+          (json/encode
+           {:foo (Date. (long 0))}))))
+  (is (= {"foo" "1970-01-01"}
+         (json/decode
+          (json/encode
+           {:foo (Date. (long 0))} "yyyy-MM-dd")))
+      "encode with given date format"))
+
+(deftest test-uuid
+  (let [id (UUID/randomUUID)
+        id-str (str id)]
+    (is (= {"foo" id-str}
+           (json/decode
+            (json/encode
+             {:foo id}))))))
+
+(deftest test-streams
+  (is (= {"foo" "bar"}
+         (json/parse-stream
+          (BufferedReader. (StringReader. "{\"foo\":\"bar\"}\n")))))
+  (let [sw (StringWriter.)
+        bw (BufferedWriter. sw)]
+    (json/generate-stream {"foo" "bar"} bw)
+    (is (= "{\"foo\":\"bar\"}" (.toString sw))))
+  (is (= {(keyword "foo baz") "bar"}
+         (with-open [rdr (StringReader. "{\"foo baz\":\"bar\"}\n")]
+           (json/parse-stream rdr true)))))
+
+(deftest test-jsondotorg-pass1
+  (let [string (slurp "test/pass1.json")
+        decoded-json (json/decode string)
+        encoded-json (json/encode decoded-json)
+        re-decoded-json (json/decode encoded-json)]
+    (is (= decoded-json re-decoded-json))))
+
+(defn timed-tests [tests]
+  (let [start (System/nanoTime)]
+    (dotimes [i 1000]
+      (doseq [t tests]
+        (t)))
+    (/ (double (- (System/nanoTime) start)) 1000000.0)))
+
+#_(deftest test-performance
+  (let [tests (->> (ns-publics 'cheshire.test.core)
+                   (remove (comp (partial = 'test-performance) first))
+                   (map second)
+                   (filter (comp :test meta)))
+        start (System/nanoTime)]
+    (let [times (doall (map timed-tests (repeat 3 tests)))
+          min-time (apply min times)
+          max-time (apply max times)]
+      (prn min-time max-time)
+      (is (> 2000 min-time))
+      (is (> 5200 max-time)))))
