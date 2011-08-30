4f40a319a61aa7be55ad4c3f96233c522fbd7058
Lee Hinman <lee@writequit.org>
2011-08-30 10:59:22 -0600

    bump to 2.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cheshire "2.0.1-SNAPSHOT"
+(defproject cheshire "2.0.1"
   :description "JSON and JSON SMILE encoding, fast."
   :url "https://github.com/dakrone/cheshire"
   :dependencies [[org.clojure/clojure "1.2.1"]
