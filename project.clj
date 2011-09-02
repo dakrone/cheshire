3d1e844a359459652268edeeb79ee59bd1709248
Lee Hinman <lee@writequit.org>
2011-09-02 16:33:19 -0600

    bump beta version

--- project.clj
+++ project.clj
@@ -9,6 +9,6 @@
   :multi-deps {"1.2.0" [[org.clojure/clojure "1.2.0"]
                         [org.codehaus.jackson/jackson-core-asl "1.8.5"]
                         [org.codehaus.jackson/jackson-smile "1.8.5"]]
-               "1.3" [[org.clojure/clojure "1.3.0-beta2"]
+               "1.3" [[org.clojure/clojure "1.3.0-beta3"]
                       [org.codehaus.jackson/jackson-core-asl "1.8.5"]
                       [org.codehaus.jackson/jackson-smile "1.8.5"]]})
