07f57656eb60de5ad4aaa194913c41550ca897ef
Lee Hinman <lee@writequit.org>
2011-09-12 21:41:21 -0600

    bump clojure 1.3 version in multi-deps

--- project.clj
+++ project.clj
@@ -9,6 +9,6 @@
   :multi-deps {"1.2.0" [[org.clojure/clojure "1.2.0"]
                         [org.codehaus.jackson/jackson-core-asl "1.8.5"]
                         [org.codehaus.jackson/jackson-smile "1.8.5"]]
-               "1.3" [[org.clojure/clojure "1.3.0-beta3"]
+               "1.3" [[org.clojure/clojure "1.3.0-RC0"]
                       [org.codehaus.jackson/jackson-core-asl "1.8.5"]
                       [org.codehaus.jackson/jackson-smile "1.8.5"]]})
