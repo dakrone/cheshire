d085c93ecd78131b701e063e4d7bdf1c174311e8
Lee Hinman <lee@writequit.org>
2011-11-07 10:52:10 -0500

    update Jackson to 1.9.2

--- project.clj
+++ project.clj
@@ -2,13 +2,13 @@
   :description "JSON and JSON SMILE encoding, fast."
   :url "https://github.com/dakrone/cheshire"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.codehaus.jackson/jackson-core-asl "1.8.5"]
-                 [org.codehaus.jackson/jackson-smile "1.8.5"]]
+                 [org.codehaus.jackson/jackson-core-asl "1.9.2"]
+                 [org.codehaus.jackson/jackson-smile "1.9.2"]]
   :dev-dependencies [[lein-marginalia "0.6.0"]
                      [lein-multi "1.0.0"]]
   :multi-deps {"1.2.1" [[org.clojure/clojure "1.2.1"]
-                        [org.codehaus.jackson/jackson-core-asl "1.8.5"]
-                        [org.codehaus.jackson/jackson-smile "1.8.5"]]
+                        [org.codehaus.jackson/jackson-core-asl "1.9.2"]
+                        [org.codehaus.jackson/jackson-smile "1.9.2"]]
                "1.4.0" [[org.clojure/clojure "1.4.0-alpha1"]
-                        [org.codehaus.jackson/jackson-core-asl "1.8.5"]
-                        [org.codehaus.jackson/jackson-smile "1.8.5"]]})
+                        [org.codehaus.jackson/jackson-core-asl "1.9.2"]
+                        [org.codehaus.jackson/jackson-smile "1.9.2"]]})
