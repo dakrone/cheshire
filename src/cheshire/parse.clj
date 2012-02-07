a2e8f25c7241f5b00ab0eff3c15c7378e7b18a94
Lee Hinman <lee@writequit.org>
2012-02-06 17:22:39 -0700

    don't bind values or instantiate objects when not needed

--- src/cheshire/parse.clj
+++ src/cheshire/parse.clj
@@ -45,16 +45,26 @@
 
 (defn parse [^JsonParser jp fst? keywords? eof array-coerce-fn]
   (let [fst? (boolean fst?)
-        keywords? (boolean keywords?)
-        x (if fst?
-            (do
-              (.nextToken jp)
-              (if (nil? (.getCurrentToken jp))
-                eof
-                (Object.)))
-            (Object.))]
-    (if (= x eof)
-      eof
+        keywords? (boolean keywords?)]
+    (if fst?
+      (do
+        (.nextToken jp)
+        (if (nil? (.getCurrentToken jp))
+          eof
+          (condp = (.getCurrentToken jp)
+            JsonToken/START_OBJECT (parse-object jp fst? keywords?
+                                                 eof array-coerce-fn)
+            JsonToken/START_ARRAY (parse-array jp fst? keywords?
+                                               eof array-coerce-fn)
+            JsonToken/VALUE_STRING (.getText jp)
+            JsonToken/VALUE_NUMBER_INT (.getNumberValue jp)
+            JsonToken/VALUE_NUMBER_FLOAT (.getDoubleValue jp)
+            JsonToken/VALUE_TRUE true
+            JsonToken/VALUE_FALSE false
+            JsonToken/VALUE_NULL nil
+            (throw
+             (Exception.
+              (str "Cannot parse " (pr-str (.getCurrentToken jp))))))))
       (condp = (.getCurrentToken jp)
         JsonToken/START_OBJECT (parse-object jp fst? keywords?
                                              eof array-coerce-fn)
