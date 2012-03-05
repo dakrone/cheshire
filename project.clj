efa5d65cb9c67684379bf4d25a07378fb9a03ba6
Lee Hinman <lee@writequit.org>
2012-03-05 09:25:38 -0500

    Bump jackson deps, use latest 1.4 beta for multi-test

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "https://github.com/dakrone/cheshire"
   :warn-on-reflection false
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.codehaus.jackson/jackson-core-asl "1.9.4"]
-                 [org.codehaus.jackson/jackson-smile "1.9.4"]]
+                 [org.codehaus.jackson/jackson-core-asl "1.9.5"]
+                 [org.codehaus.jackson/jackson-smile "1.9.5"]]
   :dev-dependencies [[lein-marginalia "0.6.0"]
                      [lein-multi "1.1.0"]]
   :multi-deps {"1.2.1" [[org.clojure/clojure "1.2.1"]
-                        [org.codehaus.jackson/jackson-core-asl "1.9.4"]
-                        [org.codehaus.jackson/jackson-smile "1.9.4"]]
-               "1.4.0" [[org.clojure/clojure "1.4.0-beta1"]
-                        [org.codehaus.jackson/jackson-core-asl "1.9.4"]
-                        [org.codehaus.jackson/jackson-smile "1.9.4"]]})
+                        [org.codehaus.jackson/jackson-core-asl "1.9.5"]
+                        [org.codehaus.jackson/jackson-smile "1.9.5"]]
+               "1.4.0" [[org.clojure/clojure "1.4.0-beta3"]
+                        [org.codehaus.jackson/jackson-core-asl "1.9.5"]
+                        [org.codehaus.jackson/jackson-smile "1.9.5"]]})
