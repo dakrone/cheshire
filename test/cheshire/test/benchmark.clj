dc05dc9519edee7823bb18ebb660e45ddc68a459
Lee Hinman <lee@writequit.org>
2012-03-08 23:05:19 -0500

    test sets and keywords in benchmarking

--- test/cheshire/test/benchmark.clj
+++ test/cheshire/test/benchmark.clj
@@ -7,10 +7,17 @@
 ;; These tests just print out results, nothing else, they also
 ;; currently don't work with clojure 1.2 (but the regular tests do)
 
-(def test-obj {"int" 3 "long" (long -2147483647) "boolean" true
-               "LongObj" (Long/parseLong "2147483647") "double" 1.23
-               "nil" nil "string" "string" "vec" [1 2 3] "map" {"a" "b"}
-               "list" (list "a" "b")})
+(def test-obj {"int" 3
+               "boolean" true
+               "LongObj" (Long/parseLong "2147483647")
+               "double" 1.23
+               "nil" nil
+               "string" "string"
+               "vec" [1 2 3]
+               "map" {"a" "b"}
+               "list" '("a" "b")
+               "set" #{"a" "b"}
+               "keyword" :foo})
 
 (deftest ^{:benchmark true} t-bench-core
   (println "---------- Core Benchmarks ----------")
