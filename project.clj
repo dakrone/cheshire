f65cb7ceb728c6905903e7e56687bd3f2fc71fdd
Lee Hinman <lee@writequit.org>
2012-03-13 12:05:16 -0600

    migrate to leiningen 2

--- project.clj
+++ project.clj
@@ -5,15 +5,12 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.codehaus.jackson/jackson-core-asl "1.9.5"]
                  [org.codehaus.jackson/jackson-smile "1.9.5"]]
-  :dev-dependencies [[lein-marginalia "0.7.0"]
-                     [lein-multi "1.1.0"]
-                     [criterium "0.2.0"]]
-  :multi-deps {"1.2.1" [[org.clojure/clojure "1.2.1"]
-                        [org.codehaus.jackson/jackson-core-asl "1.9.5"]
-                        [org.codehaus.jackson/jackson-smile "1.9.5"]]
-               "1.4.0" [[org.clojure/clojure "1.4.0-beta3"]
-                        [org.codehaus.jackson/jackson-core-asl "1.9.5"]
-                        [org.codehaus.jackson/jackson-smile "1.9.5"]]}
+  :profiles {:dev {:dependencies [[lein-marginalia "0.7.0"]
+                                  [lein-multi "1.1.0"]
+                                  [criterium "0.2.0"]]}
+             :1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0-beta4"]]}}
+  :aliases {"all" ["with-profile" "dev,1.2:dev:dev,1.4"]}
   :test-selectors {:default  #(not (:benchmark %))
                    :benchmark :benchmark
                    :all (constantly true)}
