286f661c08d0071e87424a0773976a8009524b65
Lee Hinman <lee@writequit.org>
2012-03-17 09:04:27 -0700

    add generative testing in a separate namespace

--- project.clj
+++ project.clj
@@ -7,11 +7,13 @@
                  [org.codehaus.jackson/jackson-smile "1.9.5"]]
   :profiles {:dev {:dependencies [[lein-marginalia "0.7.0"]
                                   [lein-multi "1.1.0"]
-                                  [criterium "0.2.0"]]}
+                                  [criterium "0.2.0"]
+                                  [org.clojure/test.generative "0.1.4"]]}
              :1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0-beta4"]]}}
   :aliases {"all" ["with-profile" "dev,1.2:dev:dev,1.4"]}
   :test-selectors {:default  #(not (:benchmark %))
                    :benchmark :benchmark
+                   :generative :generative
                    :all (constantly true)}
   :jvm-opts ["-Xmx512M"])
---
+++ test/cheshire/test/generative.clj
@@ -0,0 +1,50 @@
+(ns cheshire.test.generative
+  (:use [cheshire.core]
+        [clojure.test.generative]
+        [clojure.test :only [deftest is]]))
+
+(defn encode-equality [x]
+  [x (decode (encode x))])
+
+(defn encode-equality-keys [x]
+  [x (decode (encode x) true)])
+
+(defspec number-json-encoding
+  (fn [a b c] [[a b c] (decode (encode [a b c]))])
+  [^int a ^long b ^double c]
+  (is (= (first %) (last %))))
+
+(defspec bool-json-encoding
+  encode-equality
+  [^boolean a]
+  (is (= (first %) (last %))))
+
+(defspec symbol-json-encoding
+  encode-equality
+  [^symbol a]
+  (is (= (str (first %)) (last %))))
+
+(defspec keyword-json-encoding
+  encode-equality
+  [^keyword a]
+  (is (= (name (first %)) (last %))))
+
+(defspec map-json-encoding
+  encode-equality
+  [^{:tag (hash-map string (hash-map string (vec string 10) 10) 10)} a]
+  (is (= (first %) (last %))))
+
+(defspec map-keyword-json-encoding
+  encode-equality-keys
+  [^{:tag (hash-map keyword (hash-map keyword (list int 2) 2) 2)} a]
+  (is (= (first %) (last %))))
+
+(deftest ^{:generative true} t-generative
+  ;; I want the seeds to change every time, set the number higher if
+  ;; you have more than 16 CPU cores
+  (let [seeds (take 16 (repeatedly #(rand-int 1024)))]
+    (println "Seeds:" seeds)
+    (binding [*msec* 15000
+              *seeds* seeds
+              *verbose* false]
+      (doall (map deref (test-namespaces 'cheshire.test.generative))))))
