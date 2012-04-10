61d43bd59394c6fcf5bf800ef2e8c24472505d03
Lee Hinman <lee@writequit.org>
2012-04-10 15:16:53 -0600

    remove lein-multi and lein-marginalia

--- project.clj
+++ project.clj
@@ -5,9 +5,7 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [com.fasterxml.jackson.core/jackson-core "2.0.0"]
                  [com.fasterxml.jackson.dataformat/jackson-dataformat-smile "2.0.0"]]
-  :profiles {:dev {:dependencies [[lein-marginalia "0.7.0"]
-                                  [lein-multi "1.1.0"]
-                                  [criterium "0.2.0"]
+  :profiles {:dev {:dependencies [[criterium "0.2.0"]
                                   [org.clojure/test.generative "0.1.4"]]}
              :1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0-beta7"]]}}
