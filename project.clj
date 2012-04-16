d8514b7632a329353a03b1e4c60bceee547132b3
Lee Hinman <lee@writequit.org>
2012-04-16 13:53:46 -0600

    bump clojure dependency

--- project.clj
+++ project.clj
@@ -2,14 +2,14 @@
   :description "JSON and JSON SMILE encoding, fast."
   :url "https://github.com/dakrone/cheshire"
   :warn-on-reflection false
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [com.fasterxml.jackson.core/jackson-core "2.0.0"]
                  [com.fasterxml.jackson.dataformat/jackson-dataformat-smile "2.0.0"]]
   :profiles {:dev {:dependencies [[criterium "0.2.0"]
                                   [org.clojure/test.generative "0.1.4"]]}
              :1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
-             :1.4 {:dependencies [[org.clojure/clojure "1.4.0-beta7"]]}}
-  :aliases {"all" ["with-profile" "dev,1.2:dev:dev,1.4"]}
+             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}}
+  :aliases {"all" ["with-profile" "dev,1.2:dev,1.3:dev"]}
   :test-selectors {:default  #(not (:benchmark %))
                    :benchmark :benchmark
                    :generative :generative
