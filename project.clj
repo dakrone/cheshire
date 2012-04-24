ad93fc33972bf4140f3082da6981ce7491d46a33
Lee Hinman <lee@writequit.org>
2012-04-24 09:09:11 -0600

    bump criterium dependency version

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [com.fasterxml.jackson.core/jackson-core "2.0.0"]
                  [com.fasterxml.jackson.dataformat/jackson-dataformat-smile "2.0.0"]]
-  :profiles {:dev {:dependencies [[criterium "0.2.0"]
+  :profiles {:dev {:dependencies [[criterium "0.2.1"]
                                   [org.clojure/test.generative "0.1.4"]
                                   [org.clojure/data.json "0.1.2"]
                                   [clj-json "0.5.0"]]}
