44dbf9d42b52579f83d8941e4f22fef5b559a64b
Lee Hinman <lee@writequit.org>
2012-09-21 18:41:32 -0600

    custom tests should (almost always) use encode*, not encode

--- test/cheshire/test/custom.clj
+++ test/cheshire/test/custom.clj
@@ -16,38 +16,46 @@
 
 (deftest t-ratio
   (let [n 1/2]
-    (is (= (double n) (:num (json/decode (json/encode {:num n}) true))))))
+    (is (= (double n) (:num (json/decode (json/encode* {:num n}) true))))))
 
 (deftest t-long-wrap-around
-  (is (= 2147483648 (json/decode (json/encode 2147483648)))))
+  (is (= 2147483648 (json/decode (json/encode* 2147483648)))))
 
 (deftest t-bigint
   (let [n 9223372036854775808]
-    (is (= n (:num (json/decode (json/encode {:num n}) true))))))
+    (is (= n (:num (json/decode (json/encode* {:num n}) true))))))
 
 (deftest t-biginteger
   (let [n (BigInteger. "42")]
-    (is (= n (:num (json/decode (json/encode {:num n}) true))))))
+    (is (= n (:num (json/decode (json/encode* {:num n}) true))))))
 
 (deftest t-bigdecimal
   (let [n (BigDecimal. "42.5")]
-    (is (= (.doubleValue n) (:num (json/decode (json/encode {:num n}) true))))
+    (is (= (.doubleValue n) (:num (json/decode (json/encode* {:num n}) true))))
     (binding [parse/*use-bigdecimals?* true]
-      (is (= n (:num (json/decode (json/encode {:num n}) true)))))))
+      (is (= n (:num (json/decode (json/encode* {:num n}) true)))))))
 
 (deftest test-string-round-trip
   (is (= test-obj (json/decode (json/encode* test-obj)))))
 
 (deftest test-generate-accepts-float
-  (is (= "3.14" (json/encode 3.14))))
+  (is (= "3.14" (json/encode* 3.14))))
 
 (deftest test-keyword-encode
   (is (= {"key" "val"}
-         (json/decode (json/encode {:key "val"})))))
+         (json/decode (json/encode* {:key "val"})))))
 
 (deftest test-generate-set
   (is (= {"set" ["a" "b"]}
-         (json/decode (json/encode {"set" #{"a" "b"}})))))
+         (json/decode (json/encode* {"set" #{"a" "b"}})))))
+
+(deftest test-generate-empty-set
+  (is (= {"set" []}
+         (json/decode (json/encode* {"set" #{}})))))
+
+(deftest test-generate-empty-array
+  (is (= {"array" []}
+         (json/decode (json/encode* {"array" []})))))
 
 (deftest test-key-coercion
   (is (= {"foo" "bar" "1" "bat" "2" "bang" "3" "biz"}
@@ -57,11 +65,11 @@
 
 (deftest test-keywords
   (is (= {:foo "bar" :bat 1}
-         (json/decode (json/encode {:foo "bar" :bat 1}) true))))
+         (json/decode (json/encode* {:foo "bar" :bat 1}) true))))
 
 (deftest test-symbols
   (is (= {"foo" "clojure.core/map"}
-         (json/decode (json/encode {"foo" 'clojure.core/map})))))
+         (json/decode (json/encode* {"foo" 'clojure.core/map})))))
 
 (deftest test-parsed-seq
   (let [br (BufferedReader. (StringReader. "1\n2\n3\n"))]
@@ -79,26 +87,26 @@
 (deftest test-date
   (is (= {"foo" "1970-01-01T00:00:00Z"}
          (json/decode
-          (json/encode
+          (json/encode*
            {:foo (Date. (long 0))}))))
   (is (= {"foo" "1970-01-01"}
          (json/decode
-          (json/encode
+          (json/encode*
            {:foo (Date. (long 0))} {:date-format "yyyy-MM-dd"})))
       "encode with given date format"))
 
 (deftest test-sql-timestamp
   (is (= {"foo" "1970-01-01T00:00:00Z"}
-         (json/decode (json/encode {:foo (Timestamp. (long 0))}))))
+         (json/decode (json/encode* {:foo (Timestamp. (long 0))}))))
   (is (= {"foo" "1970-01-01"}
-         (json/decode (json/encode {:foo (Timestamp. (long 0))}
-                                   {:date-format "yyyy-MM-dd"})))
+         (json/decode (json/encode* {:foo (Timestamp. (long 0))}
+                                    {:date-format "yyyy-MM-dd"})))
       "encode with given date format"))
 
 (deftest test-uuid
   (let [id (UUID/randomUUID)
         id-str (str id)]
-    (is (= {"foo" id-str} (json/decode (json/encode {:foo id}))))))
+    (is (= {"foo" id-str} (json/decode (json/encode* {:foo id}))))))
 
 (deftest test-streams
   (is (= {"foo" "bar"}
@@ -121,7 +129,7 @@
 (deftest test-jsondotorg-pass1
   (let [string (slurp "test/pass1.json")
         decoded-json (json/decode string)
-        encoded-json (json/encode decoded-json)
+        encoded-json (json/encode* decoded-json)
         re-decoded-json (json/decode encoded-json)]
     (is (= decoded-json re-decoded-json))))
 
@@ -148,19 +156,21 @@
 
 (deftest test-namespaced-keywords
   (is (= "{\"foo\":\"user/bar\"}"
-         (json/encode {:foo :user/bar}))))
+         (json/encode* {:foo :user/bar})))
+  (is (= {:foo/bar "baz/eggplant"}
+         (json/decode (json/encode* {:foo/bar :baz/eggplant}) true))))
 
 (deftest test-array-coerce-fn
   (is (= {"set" #{"a" "b"} "array" ["a" "b"] "map" {"a" 1}}
          (json/decode
-          (json/encode {"set" #{"a" "b"} "array" ["a" "b"] "map" {"a" 1}}) false
+          (json/encode* {"set" #{"a" "b"} "array" ["a" "b"] "map" {"a" 1}}) false
           (fn [field-name] (if (= "set" field-name) #{} []))))))
 
 (deftest t-symbol-encoding-for-non-resolvable-symbols
   (is (= "{\"foo\":\"clojure.core/map\",\"bar\":\"clojure.core/pam\"}"
-         (json/encode {:foo 'clojure.core/map :bar 'clojure.core/pam})))
+         (json/encode* {:foo 'clojure.core/map :bar 'clojure.core/pam})))
   (is (= "{\"foo\":\"foo.bar/baz\",\"bar\":\"clojure.core/pam\"}"
-         (json/encode {:foo 'foo.bar/baz :bar 'clojure.core/pam}))))
+         (json/encode* {:foo 'foo.bar/baz :bar 'clojure.core/pam}))))
 
 (deftest t-bindable-factories
   (binding [fact/*json-factory* (fact/make-json-factory
@@ -173,19 +183,15 @@
     (is (= q (json/decode (json/encode q))))
     (is (= q (json/decode (json/encode* q))))))
 
-(deftest t-namespaced-keywords
-  (is (= {:foo/bar "baz/eggplant"}
-         (json/decode (json/encode {:foo/bar :baz/eggplant}) true))))
-
 (deftest t-pretty-print
   (is (= (str "{\n  \"foo\" : 1,\n  \"bar\" : [ {\n    \"baz\" : 2\n  }, "
               "\"quux\", [ 1, 2, 3 ] ]\n}")
-         (json/encode {:foo 1 :bar [{:baz 2} :quux [1 2 3]]}
-                      {:pretty true}))))
+         (json/encode* {:foo 1 :bar [{:baz 2} :quux [1 2 3]]}
+                       {:pretty true}))))
 
 (deftest t-unicode-escaping
   (is (= "{\"foo\":\"It costs \\u00A3100\"}"
-         (json/encode {:foo "It costs £100"} {:escape-non-ascii true}))))
+         (json/encode* {:foo "It costs £100"} {:escape-non-ascii true}))))
 
 (deftest t-custom-keyword-fn
   (is (= {:FOO "bar"} (json/decode "{\"foo\": \"bar\"}"
