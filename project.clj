b01fcc1a64e4cad172b07d1cf1d0824eaadc30f0
Lee Hinman <lee@writequit.org>
2013-03-01 09:34:42 -0700

    Bump clojure 1.5, now by default

--- project.clj
+++ project.clj
@@ -7,16 +7,16 @@
   :warn-on-reflection false
   :dependencies [[com.fasterxml.jackson.core/jackson-core "2.1.4"]
                  [com.fasterxml.jackson.dataformat/jackson-dataformat-smile "2.1.4"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.4.0"]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.0"]
                                   [org.clojure/test.generative "0.1.4"]]}
              :1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
              :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-RC17"]]}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :benchmark {:test-paths ["benchmarks"]
                          :dependencies [[criterium "0.3.1"]
                                         [org.clojure/data.json "0.2.1"]
                                         [clj-json "0.5.0"]]}}
-  :aliases {"all" ["with-profile" "dev,1.3:dev:dev,1.5"]
+  :aliases {"all" ["with-profile" "dev,1.3:dev,1.4:dev"]
             "benchmark" ["with-profile" "dev,benchmark" "test"]}
   :test-selectors {:default  #(and (not (:benchmark %))
                                    (not (:generative %)))
