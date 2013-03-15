59987bc939e9542e6462abf3e8236739ac499d71
Lee Hinman <lee@writequit.org>
2013-03-14 19:29:59 -0600

    Update clojure dep to 1.5.1

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :warn-on-reflection false
   :dependencies [[com.fasterxml.jackson.core/jackson-core "2.1.4"]
                  [com.fasterxml.jackson.dataformat/jackson-dataformat-smile "2.1.4"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.0"]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
                                   [org.clojure/test.generative "0.1.4"]]}
              :1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
              :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
