631406e1b3994d78c26aea1a1c00236a625bf892
Lee Hinman <lee@writequit.org>
2013-05-20 10:28:30 -0600

    bump tigris to 0.1.1

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :warn-on-reflection false
   :dependencies [[com.fasterxml.jackson.core/jackson-core "2.2.1"]
                  [com.fasterxml.jackson.dataformat/jackson-dataformat-smile "2.2.1"]
-                 [tigris "0.1.0"]]
+                 [tigris "0.1.1"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
                                   [org.clojure/test.generative "0.1.4"]]}
              :1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
