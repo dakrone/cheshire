d762580f50d44b6144a2367b1f0e4fcde540a6d7
Lee Hinman <lee@writequit.org>
2013-11-07 15:08:13 -0700

    bump deps

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
             :url "http://opensource.org/licenses/MIT"
             :distribution :repo}
   :warn-on-reflection false
-  :dependencies [[com.fasterxml.jackson.core/jackson-core "2.2.2"]
-                 [com.fasterxml.jackson.dataformat/jackson-dataformat-smile "2.2.2"]
+  :dependencies [[com.fasterxml.jackson.core/jackson-core "2.2.3"]
+                 [com.fasterxml.jackson.dataformat/jackson-dataformat-smile "2.2.3"]
                  [tigris "0.1.1"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
                                   [org.clojure/test.generative "0.1.4"]]}
@@ -14,8 +14,8 @@
              :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :benchmark {:test-paths ["benchmarks"]
-                         :dependencies [[criterium "0.4.1"]
-                                        [org.clojure/data.json "0.2.2"]
+                         :dependencies [[criterium "0.4.2"]
+                                        [org.clojure/data.json "0.2.3"]
                                         [clj-json "0.5.3"]]}}
   :aliases {"all" ["with-profile" "dev,1.3:dev,1.4:dev"]
             "benchmark" ["with-profile" "dev,benchmark" "test"]
@@ -26,4 +26,8 @@
                    :generative :generative
                    :all (constantly true)}
   :plugins [[codox "0.6.3"]]
-  :jvm-opts ["-Xmx512M"])
+  :jvm-opts ["-Xmx512M"
+;;             "-XX:+PrintCompilation"
+;;             "-XX:+UnlockDiagnosticVMOptions"
+;;             "-XX:+PrintInlining"
+             ])
