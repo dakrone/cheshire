18d96e1197b4ca175e08520430ebaf4585b6eea9
Vadim Geshel <vadim@yummly.com>
2014-04-07 15:56:41 -0700

    added test for extend-protocol

--- test/cheshire/test/generate.clj
+++ test/cheshire/test/generate.clj
@@ -3,12 +3,20 @@
             [cheshire.generate :as gen])
   (:use [clojure.test]))
 
-(defrecord Rec [a b c]
+(defrecord Derive [a b c]
   gen/JSONable
   (to-json [this j]
     (gen/encode-map (select-keys this [:a :b]) j)))
 
+(defrecord Extend [a b c])
+
+(extend-protocol gen/JSONable
+  Extend
+  (to-json [this j]
+    (gen/encode-map (select-keys this [:a :b]) j)))
+
 (deftest test-jsonable
-  (are [x y] (= (parse-string (generate-string x) true) y)
-       (map->Rec {:a "a" :b 1 :c "x"}) {:a "a" :b 1}
-       {:a [1 "2" (map->Rec {:a "a" :b 1 :c "x"})]} {:a [1 "2" {:a "a" :b 1}]}))
+  (doseq [constr [->Derive ->Extend]]
+    (are [x y] (= (parse-string (generate-string x) true) y)
+         (constr "a" 1 "x") {:a "a" :b 1}
+         {:a [1 "2" (constr "a" 1 "x")]} {:a [1 "2" {:a "a" :b 1}]})))
