46489a7dd5404cb81e1ab127b66b58f2aafb2a5e
Lee Hinman <lee@writequit.org>
2017-04-19 21:15:06 -0600

    Release 5.7.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cheshire "5.7.1-SNAPSHOT"
+(defproject cheshire "5.7.1"
   :description "JSON and JSON SMILE encoding, fast."
   :url "https://github.com/dakrone/cheshire"
   :license {:name "The MIT License"
