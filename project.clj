ae08ca34a1fe2de9ee13052d1efd4534479c1642
Lee Hinman <lee@writequit.org>
2022-05-26 14:19:58 -0600

    Bump jackson dependencies to 2.13.3
    Resolves #190

--- project.clj
+++ project.clj
@@ -5,24 +5,25 @@
             :url "http://opensource.org/licenses/MIT"
             :distribution :repo}
   :global-vars {*warn-on-reflection* false}
-  :dependencies [[com.fasterxml.jackson.core/jackson-core "2.12.4"]
-                 [com.fasterxml.jackson.dataformat/jackson-dataformat-smile "2.12.4"
+  :dependencies [[com.fasterxml.jackson.core/jackson-core "2.13.3"]
+                 [com.fasterxml.jackson.dataformat/jackson-dataformat-smile "2.13.3"
                   :exclusions [com.fasterxml.jackson.core/jackson-databind]]
-                 [com.fasterxml.jackson.dataformat/jackson-dataformat-cbor "2.12.4"
+                 [com.fasterxml.jackson.dataformat/jackson-dataformat-cbor "2.13.3"
                   :exclusions [com.fasterxml.jackson.core/jackson-databind]]
                  [tigris "0.1.2"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.3"]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.11.1"]
                                   [org.clojure/test.generative "0.1.4"]
                                   [org.clojure/tools.namespace "0.3.1"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
+             :1.10 {:dependencies [[org.clojure/clojure "1.10.3"]]}
              :benchmark {:test-paths ["benchmarks"]
                          :jvm-opts ^:replace ["-Xms1g" "-Xmx1g" "-server"]
                          :dependencies [[criterium "0.4.6"]
                                         [org.clojure/data.json "0.2.6"]
                                         [clj-json "0.5.3"]]}}
-  :aliases {"all" ["with-profile" "dev,1.7:dev,1.8:dev,1.9:dev"]
+  :aliases {"all" ["with-profile" "dev,1.7:dev,1.8:dev,1.9:dev,1.10:dev"]
             "benchmark" ["with-profile" "dev,benchmark" "test"]
             "pretty-bench" ["with-profile" "dev,benchmark" "test" ":only"
                           "cheshire.test.benchmark/t-bench-pretty"]
