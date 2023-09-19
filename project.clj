9ba8096545cf852a1ae56a22c83fe202648bfe0f
Lee Hinman <lee@writequit.org>
2023-09-19 20:23:29 +0100

    Bump to 5.12.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cheshire "5.12.0"
+(defproject cheshire "5.12.1-SNAPSHOT"
   :description "JSON and JSON SMILE encoding, fast."
   :url "https://github.com/dakrone/cheshire"
   :license {:name "The MIT License"
