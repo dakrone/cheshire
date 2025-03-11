9c65fed973de4027aaa9968336bc46f03d155c73
lread <lee@dlread.com>
2025-03-11 15:19:18 -0400

    tests: add clojure 1.12 to tested versions
    Also update testing for clojure 1.11 to latest release

--- project.clj
+++ project.clj
@@ -11,13 +11,14 @@
                  [com.fasterxml.jackson.dataformat/jackson-dataformat-cbor "2.17.0"
                   :exclusions [com.fasterxml.jackson.core/jackson-databind]]
                  [tigris "0.1.2"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.11.2"]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.12.0"]
                                   [org.clojure/test.generative "0.1.4"]
                                   [org.clojure/tools.namespace "0.3.1"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
              :1.10 {:dependencies [[org.clojure/clojure "1.10.3"]]}
+             :1.11 {:dependencies [[org.clojure/clojure "1.11.4"]]}
              :benchmark {:test-paths ["benchmarks"]
                          :jvm-opts ^:replace ["-Xms1g" "-Xmx1g" "-server"]
                          :dependencies [[criterium "0.4.6"]
